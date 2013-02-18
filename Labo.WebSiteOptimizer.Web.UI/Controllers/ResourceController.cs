fd7ca93a64e296fcda2233d8e4e024e7bb688db0
Bora Akgun <boraakgn@gmail.com>
2013-02-18 12:01:26 +0200

    added resourcecontroller

---
+++ Labo.WebSiteOptimizer.Web.UI/Controllers/ResourceController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using Labo.WebSiteOptimizer.ResourceManagement;
+
+namespace Labo.WebSiteOptimizer.Web.UI.Controllers
+{
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class ResourceController : AsyncController
+    {
+        public void JsAsync(string name)
+        {
+            AsyncManager.OutstandingOperations.Increment();
+
+            Task.Factory.StartNew(() => ProcessJs(name));
+        }
+
+        private void ProcessJs(string name)
+        {
+            ResourceManagerRuntime.ResourceHandler.HandleResource(ControllerContext.HttpContext, ResourceType.Js, name);
+            AsyncManager.OutstandingOperations.Decrement();
+        }
+
+        public void JsCompleted()
+        {
+        }
+
+        public void CssAsync(string name)
+        {
+            AsyncManager.OutstandingOperations.Increment();
+
+            Task.Factory.StartNew(() => ProcessCss(name));
+        }
+
+        private void ProcessCss(string name)
+        {
+            ResourceManagerRuntime.ResourceHandler.HandleResource(ControllerContext.HttpContext, ResourceType.Css, name);
+            AsyncManager.OutstandingOperations.Decrement();
+        }
+
+        public void CssCompleted()
+        {
+        }
+    }
+}
