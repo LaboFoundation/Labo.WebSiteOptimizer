78b0f9a36b66d301016d90083ffdf72bcc049716
Bora Akgun <boraakgn@gmail.com>
2014-04-09 15:53:52 +0300

    .

---
+++ Labo.WebSiteOptimizer.Mvc4/HtmlHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Labo.WebSiteOptimizer.Mvc4
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcControlsFactory WebSiteOptimizer(this HtmlHelper htmlHelper)
+        {
+            return new MvcControlsFactory(htmlHelper);
+        }
+    }
+}
